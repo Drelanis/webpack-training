c54bbfbba57d6be105eb172222648a5e6b66dc88
Drelanis <denisbadakaty51@gmail.com>
2023-04-08 16:56:11 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = (env, argv) => {
