599bb88f7d0cc179ea698fdf5826b40b90631857
Drelanis <denisbadakaty51@gmail.com>
2023-04-10 14:33:28 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,29 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require('path');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-  const config = {
-    entry: './src/index.jsx',
-    output: {
-      filename: 'bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /.jsx?$/,
-          use: ['babel-loader'],
-        },
-        {
-          test: /.s?css$/,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader',
-            'sass-loader',
-          ],
-        },
-        {
-          test: /.(jpg|png)$/,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 8,
-                name: '[name].[ext]',
-                outputPath: 'images',
-              },
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              outputPath: 'images',
+              limit: 8192,
             },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new webpack.ProgressPlugin(),
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
+          },
+        ],
+      },
     ],
-    devServer: {
-      hot: true,
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-  };
-
-  if (isProduction) {
-    config.plugins.push(
-      new MiniCssExtractPlugin({
-        filename: '[name].css',
-      })
-    );
-  }
-
-  return config;
+  },
 };
